f039c1b67d3856bca395d2ad48f8b7d8f44212a3
truong172266 <lazatsssv2@gmail.com>
2021-11-23 16:53:55 +0700

    abc

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
+    <Test abc ="abc " />
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
@@ -19,6 +21,7 @@
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
+
     <h3>Ecosystem</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
@@ -31,11 +34,17 @@
 </template>
 
 <script>
+
+import Test from "@/components/Test";
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+  components: {
+    Test,
+  },
 }
 </script>
 
---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>abcdefgh {{abc}}</h1>
+</template>
+
+<script>
+export default {
+name: "Test",
+  props:{
+  abc : String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
